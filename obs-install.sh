b14899105c3caff51bf2b90366cbb26b785ca7a9
Martin Wimpress <code@flexion.org>
2022-02-14 10:50:14 +0000

    Update Source Dock to 0.3.2

--- obs-install.sh
+++ obs-install.sh
@@ -241,7 +241,7 @@ install_exeldro_plugin "https://obsproject.com/forum/resources/replay-source.686
 install_exeldro_plugin "https://obsproject.com/forum/resources/scene-collection-manager.1434/version/3992/download?file=79509" "scene-collection-manager-0.0.4-linux64.tar.gz.zip"
 install_exeldro_plugin "https://obsproject.com/forum/resources/scene-notes-dock.1398/version/3938/download?file=78708" "scene-notes-dock-0.0.3-linux64.tar.gz.zip"
 install_exeldro_plugin "https://obsproject.com/forum/resources/source-copy.1261/version/3410/download?file=71877" "source-copy-0.1.3-linux64.tar.gz.zip"
-install_exeldro_plugin "https://obsproject.com/forum/resources/source-dock.1317/version/3952/download?file=78909" "source-dock-0.2.4-linux64.tar.gz.zip"
+install_exeldro_plugin "https://obsproject.com/forum/resources/source-dock.1317/version/3987/download?file=79453" "source-dock-0.3.2-linux64.tar.gz.zip"
 install_exeldro_plugin "https://obsproject.com/forum/resources/source-record.1285/version/3726/download?file=75975" "source-record-0.2.6-linux64.tar.gz.zip"
 install_exeldro_plugin "https://obsproject.com/forum/resources/source-switcher.941/version/3349/download?file=70933" "source-switcher-0.3.0-linux64.tar.gz.zip"
 install_exeldro_plugin "https://obsproject.com/forum/resources/time-warp-scan.1167/version/3475/download?file=72760" "time-warp-scan-0.1.6-linux64.tar.gz.zip"
