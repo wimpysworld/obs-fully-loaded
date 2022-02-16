626fe695b893cf16bed5549e235a5e545c5e31f9
Martin Wimpress <code@flexion.org>
2022-02-16 20:13:26 +0000

    Update Scene Notes Dock to 0.0.4

--- obs-install.sh
+++ obs-install.sh
@@ -241,7 +241,7 @@ install_exeldro_plugin "https://obsproject.com/forum/resources/move-transition.9
 install_exeldro_plugin "https://obsproject.com/forum/resources/recursion-effect.1008/version/3928/download?file=78616" "recursion-effect-0.0.4-linux64.tar.gz.zip"
 install_exeldro_plugin "https://obsproject.com/forum/resources/replay-source.686/version/3441/download?file=72289" "replay-source-1.6.9-linux64.tar.gz.zip"
 install_exeldro_plugin "https://obsproject.com/forum/resources/scene-collection-manager.1434/version/3992/download?file=79509" "scene-collection-manager-0.0.4-linux64.tar.gz.zip"
-install_exeldro_plugin "https://obsproject.com/forum/resources/scene-notes-dock.1398/version/3938/download?file=78708" "scene-notes-dock-0.0.3-linux64.tar.gz.zip"
+install_exeldro_plugin "https://obsproject.com/forum/resources/scene-notes-dock.1398/version/4036/download?file=80203" "scene-notes-dock-0.0.4-linux64.tar.gz.zip"
 install_exeldro_plugin "https://obsproject.com/forum/resources/source-copy.1261/version/3410/download?file=71877" "source-copy-0.1.3-linux64.tar.gz.zip"
 install_exeldro_plugin "https://obsproject.com/forum/resources/source-dock.1317/version/3987/download?file=79453" "source-dock-0.3.2-linux64.tar.gz.zip"
 install_exeldro_plugin "https://obsproject.com/forum/resources/source-record.1285/version/3998/download?file=79616" "source-record-0.2.7-linux64.tar.gz.zip"
