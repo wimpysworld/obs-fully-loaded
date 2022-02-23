25da3c35621f9dad16764b71e769dcb4caf0a836
Martin Wimpress <code@flexion.org>
2022-02-23 16:11:53 +0000

    Update Teleport to 0.2.4

--- obs-install.sh
+++ obs-install.sh
@@ -261,7 +261,7 @@ install_tarball_plugin "https://github.com/Qufyy/obs-scale-to-sound/releases/dow
 install_zip_plugin "https://github.com/univrsal/dvds3/releases/download/v1.1/dvd-screensaver.v1.1.linux.x64.zip"
 install_zip_plugin "https://github.com/fzwoch/obs-gstreamer/releases/download/v0.3.3/obs-gstreamer.zip" "obs-gstreamer.zip" "linux/*"
 install_zip_plugin "https://obsproject.com/forum/resources/obs-nvfbc.796/download" "obs-nvfbc.zip"
-install_zip_plugin "https://github.com/fzwoch/obs-teleport/releases/download/0.2.3/obs-teleport.zip" "obs-teleport.zip" "linux-x86_64/*"
+install_zip_plugin "https://github.com/fzwoch/obs-teleport/releases/download/0.2.4/obs-teleport.zip" "obs-teleport.zip" "linux-x86_64/*"
 install_zip_plugin "https://obsproject.com/forum/resources/rgb-levels.967/download" "rgb-levels-linux.zip"
 install_zip_plugin "https://github.com/univrsal/spectralizer/releases/download/v1.3.4/spectralizer.v1.3.4.bin.linux.x64.zip"
 install_zip_plugin "https://github.com/Xaymar/obs-StreamFX/releases/download/0.11.0/streamfx-ubuntu-20.04-0.11.0.0-g31d56703.zip"
