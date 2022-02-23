7dadb64f81c8fa66899e91624ccca1f2afe03b6a
Martin Wimpress <code@flexion.org>
2022-02-23 16:19:35 +0000

    Version zip files

--- obs-install.sh
+++ obs-install.sh
@@ -259,13 +259,13 @@ install_tarball_plugin "https://github.com/Qufyy/obs-scale-to-sound/releases/dow
 
 # Install Zipped plugins to ~/.config/obs-studio/plugins
 install_zip_plugin "https://github.com/univrsal/dvds3/releases/download/v1.1/dvd-screensaver.v1.1.linux.x64.zip"
-install_zip_plugin "https://github.com/fzwoch/obs-gstreamer/releases/download/v0.3.3/obs-gstreamer.zip" "obs-gstreamer.zip" "linux/*"
-install_zip_plugin "https://obsproject.com/forum/resources/obs-nvfbc.796/download" "obs-nvfbc.zip"
-install_zip_plugin "https://github.com/fzwoch/obs-teleport/releases/download/0.2.4/obs-teleport.zip" "obs-teleport.zip" "linux-x86_64/*"
+install_zip_plugin "https://github.com/fzwoch/obs-gstreamer/releases/download/v0.3.3/obs-gstreamer.zip" "obs-gstreamer-v0.3.3.zip" "linux/*"
+install_zip_plugin "https://obsproject.com/forum/resources/obs-nvfbc.796/download" "obs-nvfbc-0.0.5.zip"
+install_zip_plugin "https://github.com/fzwoch/obs-teleport/releases/download/0.2.4/obs-teleport.zip" "obs-teleport-0.2.4.zip" "linux-x86_64/*"
 install_zip_plugin "https://obsproject.com/forum/resources/rgb-levels.967/download" "rgb-levels-linux.zip"
 install_zip_plugin "https://github.com/univrsal/spectralizer/releases/download/v1.3.4/spectralizer.v1.3.4.bin.linux.x64.zip"
 install_zip_plugin "https://github.com/Xaymar/obs-StreamFX/releases/download/0.11.0/streamfx-ubuntu-20.04-0.11.0.0-g31d56703.zip"
-install_zip_plugin "https://github.com/WarmUpTill/SceneSwitcher/releases/download/1.15.3/SceneSwitcher.zip" "SceneSwitcher.zip" "SceneSwitcher/Linux/advanced-scene-switcher/*"
+install_zip_plugin "https://github.com/WarmUpTill/SceneSwitcher/releases/download/1.15.3/SceneSwitcher.zip" "SceneSwitcher-1.15.3.zip" "SceneSwitcher/Linux/advanced-scene-switcher/*"
 
 # Install Zipped theme to ~/.config/obs-studio/themes
 install_theme "https://github.com/cssmfc/camgirl-obs/releases/download/1.1.OBS.CGC/cgc_theme_obs.zip"
